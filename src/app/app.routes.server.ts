1e5e1c317b1690986b7379217d2fc23f6e8a8420
HaythamNaguib <haytham121992@gmail.com>
2026-03-31 02:31:00 +0200

    fix build errors

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,15 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'details/:slug/:id',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'details/:id',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
